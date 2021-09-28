3920517f1c36e964c0485c655d691d8e8be79476
AleksisZauers <73470982+AleksisZauers@users.noreply.github.com>
2021-09-28 14:25:25 +0300

    Add files via upload

---
+++ work.js
@@ -0,0 +1,12 @@
+
+
+var koki= [
+
+    {"koki":"Egle", garums:5, vecums: 2},
+    {"koki":"Ozols", garums:22,  vecums: 5},
+    {"koki":"Priede", garums:90,  vecums: 10}
+];
+
+for(var i=0;i<koki.length;i++){
+    console.log(koki[i].koki+koki[i].garums+koki[i].vecums)
+}
